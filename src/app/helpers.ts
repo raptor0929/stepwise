738d911404d34654d4e8525fb075e01da55e8114
raptor0929 <flaura.dev@gmail.com>
2025-06-14 07:09:26 -0400

    update bytes32 convertion

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,4 +1,5 @@
-// 1D0C697F-D20B-48FD-8658-978B970756F9 -> bytes32
-export const sahhaIdToBytes32 = (sahhaId: string) => {
-  return `0x${sahhaId.replace(/-/g, '').slice(0, 32)}`;
+export const sahhaIdToBytes32 = (sahhaId: string): `0x${string}` => {
+  let hex = sahhaId.replace(/-/g, "");
+  hex = hex.padEnd(64, "0");
+  return `0x${hex}` as `0x${string}`;
 };
